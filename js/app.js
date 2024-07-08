db9eb3ebf070660fdd78bda3339fbbcf1b76a4e3
BaliGabi <baligabor81@gmail.com>
2024-07-08 11:32:59 +0200

    debug

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 async function loadData() {
     try {
-        const response = await fetch('../datas/datas.json');
+        const response = await fetch('/datas/datas.json');
+        console.log(response);
         const data = await response.json();
 
         const categories = [...new Set(data.map(item => item.category))];
